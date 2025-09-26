439bc91576393a30e95a2f18eaf95270c2ff5d8a
Luke Atkinson-Coyle <luke.atkinson-coyle@outlook.com>
2025-09-26 12:01:21 +0100

    bump mantine

--- components/AppHeader/ColorModeToggle/ColorModeToggle.tsx
+++ components/AppHeader/ColorModeToggle/ColorModeToggle.tsx
@@ -1,43 +1,36 @@
-import { ColorPicker, Popover, Button } from "@mantine/core";
-import { IconColorPicker } from "@tabler/icons-react";
+"use client";
+import { Button } from "@mantine/core";
+import { useCallback, useState } from "react";
+
+const colors = [
+  "#ffa8a8", // --mantine-color-red-3,
+  "#faa2c1", // --mantine-color-pink-3,
+  "#e599f7", // --mantine-color-grape-3,
+  "#b197fc", // --mantine-color-violet-3,
+  "#91a7ff", // --mantine-color-indigo-3,
+  "#8ce99a", // --mantine-color-green-3,
+  "#74c0fc", // --mantine-color-blue-3,
+  "#66d9e8", // --mantine-color-cyan-3,
+  "#63e6be", // --mantine-color-teal-3,
+  "#c0eb75", // --mantine-color-lime-3,
+  "#ffe066", // --mantine-color-yellow-3,
+  "#ffc078", // --mantine-color-orange-3,
+];
 
 export function ColorModeToggle() {
-  const changeColorMode = (color: string) => {
-    document.documentElement.dataset.colorMode = color;
-  };
+  const [increment, setIncrement] = useState(0);
+
+  const changeColorMode = useCallback(() => {
+    setIncrement((prev) => (prev >= colors.length - 1 ? 0 : prev + 1));
+    console.log(colors[increment], increment, colors.length);
+    document.documentElement.dataset.colorMode = colors[increment];
+  }, [increment]);
 
   return (
-    <Popover width={500} position="bottom" withArrow shadow="md">
-      <Popover.Target>
-        <Button
-          aria-label="Toggle ColorMode"
-          style={{ background: "var(--colorMode-color)" }}
-        >
-          <IconColorPicker />
-        </Button>
-      </Popover.Target>
-      <Popover.Dropdown>
-        <ColorPicker
-          aria-label="ColorMode Popover"
-          onChange={changeColorMode}
-          withPicker={false}
-          fullWidth
-          swatches={[
-            "#ffa8a8", // --mantine-color-red-3,
-            "#faa2c1", // --mantine-color-pink-3,
-            "#e599f7", // --mantine-color-grape-3,
-            "#b197fc", // --mantine-color-violet-3,
-            "#91a7ff", // --mantine-color-indigo-3,
-            "#8ce99a", // --mantine-color-green-3,
-            "#74c0fc", // --mantine-color-blue-3,
-            "#66d9e8", // --mantine-color-cyan-3,
-            "#63e6be", // --mantine-color-teal-3,
-            "#c0eb75", // --mantine-color-lime-3,
-            "#ffe066", // --mantine-color-yellow-3,
-            "#ffc078", // --mantine-color-orange-3,
-          ]}
-        />
-      </Popover.Dropdown>
-    </Popover>
+    <Button
+      aria-label="Toggle ColorMode"
+      style={{ backgroundColor: "var(--colorMode-color)" }}
+      onClick={changeColorMode}
+    />
   );
 }
